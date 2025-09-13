d47836e206951297fb117f2b5cf6ddc1620b9e58
Eric Fitzgerald <github@efitz.net>
2025-09-13 02:22:48 -0400

    Restoring regex functionality

--- src/app/pages/dfd/services/dfd-event-handlers.service.ts
+++ src/app/pages/dfd/services/dfd-event-handlers.service.ts
@@ -715,8 +715,11 @@ export class DfdEventHandlersService {
     sanitized = sanitized.replace(/\s+/g, ' ');
 
     // Remove control characters but keep newlines for multi-line labels
+    // First remove ASCII control chars (except \t, \n, \r)
     // eslint-disable-next-line no-control-regex
     sanitized = sanitized.replace(/[\x00-\x08\x0B\x0C\x0E-\x1F\x7F]/g, '');
+    // Then remove Unicode control chars but preserve newlines
+    sanitized = sanitized.replace(/\p{Cc}/gu, (match) => /[\n\r]/.test(match) ? match : '');
 
     return sanitized;
   }
