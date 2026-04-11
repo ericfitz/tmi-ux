0eb62de0df8c0409f963818f6f857c410f0c5551
Eric Fitzgerald <github@efitz.net>
2026-04-11 00:27:48 -0400

    test: register Phase 1 page objects, dialogs, and flows in fixtures

--- e2e/fixtures/test-fixtures.ts
+++ e2e/fixtures/test-fixtures.ts
@@ -12,10 +12,26 @@ import { DeleteConfirmDialog } from '../dialogs/delete-confirm.dialog';
 import { ThreatEditorDialog } from '../dialogs/threat-editor.dialog';
 import { CvssCalculatorDialog } from '../dialogs/cvss-calculator.dialog';
 import { CwePickerDialog } from '../dialogs/cwe-picker.dialog';
+import { NotePage } from '../pages/note-page.page';
+import { AssetEditorDialog } from '../dialogs/asset-editor.dialog';
+import { DocumentEditorDialog } from '../dialogs/document-editor.dialog';
+import { RepositoryEditorDialog } from '../dialogs/repository-editor.dialog';
+import { MetadataDialog } from '../dialogs/metadata.dialog';
+import { PermissionsDialog } from '../dialogs/permissions.dialog';
+import { SsvcCalculatorDialog } from '../dialogs/ssvc-calculator.dialog';
+import { ExportDialog } from '../dialogs/export.dialog';
+import { FrameworkMappingPickerDialog } from '../dialogs/framework-mapping-picker.dialog';
 import { AuthFlow } from '../flows/auth.flow';
 import { ThreatModelFlow } from '../flows/threat-model.flow';
 import { ThreatFlow } from '../flows/threat.flow';
 import { DiagramFlow } from '../flows/diagram.flow';
+import { AssetFlow } from '../flows/asset.flow';
+import { DocumentFlow } from '../flows/document.flow';
+import { RepositoryFlow } from '../flows/repository.flow';
+import { NoteFlow } from '../flows/note.flow';
+import { MetadataFlow } from '../flows/metadata.flow';
+import { PermissionsFlow } from '../flows/permissions.flow';
+import { ScoringFlow } from '../flows/scoring.flow';
 
 type TestFixtures = {
   // Pages
@@ -26,6 +42,7 @@ type TestFixtures = {
   triagePage: TriagePage;
   loginPage: LoginPage;
   navbarPage: NavbarPage;
+  notePage: NotePage;
 
   // Dialogs
   createTmDialog: CreateTmDialog;
@@ -34,12 +51,27 @@ type TestFixtures = {
   threatEditorDialog: ThreatEditorDialog;
   cvssCalculatorDialog: CvssCalculatorDialog;
   cwePickerDialog: CwePickerDialog;
+  assetEditorDialog: AssetEditorDialog;
+  documentEditorDialog: DocumentEditorDialog;
+  repositoryEditorDialog: RepositoryEditorDialog;
+  metadataDialog: MetadataDialog;
+  permissionsDialog: PermissionsDialog;
+  ssvcCalculatorDialog: SsvcCalculatorDialog;
+  exportDialog: ExportDialog;
+  frameworkMappingPickerDialog: FrameworkMappingPickerDialog;
 
   // Flows
   authFlow: AuthFlow;
   threatModelFlow: ThreatModelFlow;
   threatFlow: ThreatFlow;
   diagramFlow: DiagramFlow;
+  assetFlow: AssetFlow;
+  documentFlow: DocumentFlow;
+  repositoryFlow: RepositoryFlow;
+  noteFlow: NoteFlow;
+  metadataFlow: MetadataFlow;
+  permissionsFlow: PermissionsFlow;
+  scoringFlow: ScoringFlow;
 };
 
 export const test = base.extend<TestFixtures>({
@@ -65,6 +97,9 @@ export const test = base.extend<TestFixtures>({
   navbarPage: async ({ page }, use) => {
     await use(new NavbarPage(page));
   },
+  notePage: async ({ page }, use) => {
+    await use(new NotePage(page));
+  },
 
   // Dialogs
   createTmDialog: async ({ page }, use) => {
@@ -85,6 +120,30 @@ export const test = base.extend<TestFixtures>({
   cwePickerDialog: async ({ page }, use) => {
     await use(new CwePickerDialog(page));
   },
+  assetEditorDialog: async ({ page }, use) => {
+    await use(new AssetEditorDialog(page));
+  },
+  documentEditorDialog: async ({ page }, use) => {
+    await use(new DocumentEditorDialog(page));
+  },
+  repositoryEditorDialog: async ({ page }, use) => {
+    await use(new RepositoryEditorDialog(page));
+  },
+  metadataDialog: async ({ page }, use) => {
+    await use(new MetadataDialog(page));
+  },
+  permissionsDialog: async ({ page }, use) => {
+    await use(new PermissionsDialog(page));
+  },
+  ssvcCalculatorDialog: async ({ page }, use) => {
+    await use(new SsvcCalculatorDialog(page));
+  },
+  exportDialog: async ({ page }, use) => {
+    await use(new ExportDialog(page));
+  },
+  frameworkMappingPickerDialog: async ({ page }, use) => {
+    await use(new FrameworkMappingPickerDialog(page));
+  },
 
   // Flows
   authFlow: async ({ page }, use) => {
@@ -98,6 +157,27 @@ export const test = base.extend<TestFixtures>({
   },
   diagramFlow: async ({ page }, use) => {
     await use(new DiagramFlow(page));
+  },
+  assetFlow: async ({ page }, use) => {
+    await use(new AssetFlow(page));
+  },
+  documentFlow: async ({ page }, use) => {
+    await use(new DocumentFlow(page));
+  },
+  repositoryFlow: async ({ page }, use) => {
+    await use(new RepositoryFlow(page));
+  },
+  noteFlow: async ({ page }, use) => {
+    await use(new NoteFlow(page));
+  },
+  metadataFlow: async ({ page }, use) => {
+    await use(new MetadataFlow(page));
+  },
+  permissionsFlow: async ({ page }, use) => {
+    await use(new PermissionsFlow(page));
+  },
+  scoringFlow: async ({ page }, use) => {
+    await use(new ScoringFlow(page));
   },
 });
 
